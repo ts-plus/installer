a0bfbcc94e1e46c2ca87d7e04114c14bac20239d
Michael Arnaldi <arnaldimichael@gmail.com>
2022-01-29 00:23:07 +0000

    chore: add eslint

--- src/download.ts
+++ src/download.ts
@@ -1,26 +1,27 @@
-import * as https from 'https';
-import * as fs from 'fs';
-import * as rimraf from 'rimraf';
-import * as path from 'path';
-import * as mkdirp from 'mkdirp';
+import * as fs from "fs"
+import * as https from "https"
+import * as mkdirp from "mkdirp"
+import * as path from "path"
+import * as rimraf from "rimraf"
 
-const tgz = "https://raw.githubusercontent.com/ts-plus/typescript/main/dev/typescript-4.6.0-ets.20220128.tgz"
+const tgz =
+  "https://raw.githubusercontent.com/ts-plus/typescript/main/dev/typescript-4.6.0-ets.20220128.tgz"
 
 const target = path.join(__dirname, "..", "/compiler")
 
 rimraf(target, (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        mkdirp(target).then(() => {
-            const tsPath = path.join(target, "/typescript.tgz");
-            const file = fs.createWriteStream(tsPath);
+  if (err) {
+    console.error(err)
+  } else {
+    mkdirp(target).then(() => {
+      const tsPath = path.join(target, "/typescript.tgz")
+      const file = fs.createWriteStream(tsPath)
 
-            https.get(tgz, (response) => {
-                response.pipe(file).on("finish", () => {
-                    file.close();
-                });
-            });
+      https.get(tgz, (response) => {
+        response.pipe(file).on("finish", () => {
+          file.close()
         })
-    }
+      })
+    })
+  }
 })
--- src/install.ts
+++ src/install.ts
@@ -1,20 +1,19 @@
-import * as rimraf from 'rimraf';
-import * as path from 'path';
-import * as tar from 'tar';
-import * as mkdirp from 'mkdirp';
+import * as mkdirp from "mkdirp"
+import * as path from "path"
+import * as rimraf from "rimraf"
+import * as tar from "tar"
 
 const target = path.join(__dirname, "..", "/compiler")
-const tsPath = path.join(target, "/typescript.tgz");
+const tsPath = path.join(target, "/typescript.tgz")
 
-const originalTsFolder = path.join(
-    require.resolve("typescript"),
-    "../../"
-)
+const originalTsFolder = path.join(require.resolve("typescript"), "../../")
 
 rimraf(originalTsFolder, () => {
-    mkdirp(originalTsFolder).then(() => tar.extract({
-        file: tsPath,
-        cwd: originalTsFolder,
-        strip: 1
-    }))
+  mkdirp(originalTsFolder).then(() =>
+    tar.extract({
+      file: tsPath,
+      cwd: originalTsFolder,
+      strip: 1
+    })
+  )
 })
