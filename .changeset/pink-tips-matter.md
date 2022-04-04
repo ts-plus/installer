660e963cf18713fecf68d2c886f099c297e33401
Michael Arnaldi <arnaldimichael@gmail.com>
2022-04-04 13:06:06 +0000

    chore: update release

---
+++ .changeset/pink-tips-matter.md
@@ -0,0 +1,5 @@
+---
+"@tsplus/installer": patch
+---
+
+Fix local unique names in variable exports (https://github.com/ts-plus/typescript/pull/127)
--- prepack.sh
+++ prepack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-COMMIT="89fdfd34056be1ac9f5a1f1f5c84e5f59b08ced4"
+COMMIT="3d101a0d01630d6d9920af6fb2bbee9865ce71bb"
 rm -rf tmp
 mkdir tmp
 cd tmp
