fd69d27a0cad28fb6938575399c863cd30b0f360
0x706b <peter@peterkrol.com>
2022-06-19 06:49:09 -0400

    chore: update release

---
+++ .changeset/odd-knives-shake.md
@@ -0,0 +1,5 @@
+---
+"@tsplus/installer": patch
+---
+
+chore: turn on debugging in replay.io
--- prepack.sh
+++ prepack.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-COMMIT="ffd46c810652758986c67e204de3988c8a1b92b9"
+COMMIT="a1bbfcb82607863ec7177627fd17b7d6a64cb6fd"
 rm -rf tmp
 mkdir tmp
 cd tmp
